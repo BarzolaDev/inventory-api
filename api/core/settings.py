c7d09013fa17cf8a3a5900a226aff1aeed8cd68f
Maximiliano <stuckeedd@gmail.com>
2026-04-22 05:59:24 -0300

    chore: remove hardcoded TEST_POSTGRES_URL

--- api/core/settings.py
+++ api/core/settings.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    TEST_POSTGRES_URL: str = "postgresql://user:password@localhost:5432/dbname"
 
     model_config = SettingsConfigDict(env_file=".env")
 
