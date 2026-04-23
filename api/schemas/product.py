e7d1b601cdfc49d8b1a4380a6c3c1c300713f9a4
Maximiliano <stuckeedd@gmail.com>
2026-04-23 15:30:39 -0300

    refactor: improve validation error message in ProductBase

--- api/schemas/product.py
+++ api/schemas/product.py
@@ -7,10 +7,13 @@ class ProductBase(BaseModel):
     unit: str = Field(min_length=1, max_length=20)
     purchase_price: Decimal = Field(ge=0, decimal_places=2)
     sale_price: Decimal = Field(ge=0, decimal_places=2)
+
+
+    # 🔹 Validaciones
     @model_validator(mode="after")
     def sale_price_must_be_grater(self):
         if self.sale_price <= self.purchase_price:
-            raise ValueError("sale_price must be greater than purchase_price")
+            raise ValueError("Selling price must be greater than purchase price")
         return self
 
 class ProductCreate(ProductBase):
